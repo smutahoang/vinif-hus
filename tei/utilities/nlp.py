5ff321565f07706a5765978e63aeac7741ee58b2
vinif hus <vinif.hus@cefd-login.cluster>
2020-12-23 19:39:17 +0700

    del tei

--- tei/utilities/nlp.py
+++
@@ -1,9 +0,0 @@
-def get_relevant_sentence(document, post):
-    """
-    to extract sentences from a document that are relevant to a post
-    :param document: list of string, each is a paragraph, i.e., containing multiple sentences
-    :param post: string
-    :return: list of strings, each is a relevant sentence
-    """
-    # TODO: to be implemented
-    pass

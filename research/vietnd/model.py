fe57fea69608e48ac85249a414a13a924280c2b6
VietNDuc <viet.nguyen1123@gmail.com>
2021-01-07 11:45:40 +0700

    Change to batch processing

--- research/vietnd/model.py
+++ research/vietnd/model.py
@@ -1,9 +1,32 @@
+import os
+
 from vncorenlp import VnCoreNLP
 import torch
+import torch.nn.functional as F
 from transformers import RobertaTokenizer, RobertaModel
 from transformers import AutoModel, AutoTokenizer
 
-from utils import flatten
+from utils import flatten, checkvncorenlp
+
+
+# PhoBert large
+model_phobert_large = AutoModel.from_pretrained("vinai/phobert-large",
+                                                return_dict=True,
+                                                output_hidden_states=True)
+tokenizer_phobert_large = AutoTokenizer.from_pretrained("vinai/phobert-large",
+                                                        use_fast=False)
+
+# XLM-R large
+tokenizer_xlmr_large = RobertaTokenizer.from_pretrained('roberta-large')
+model_xlmr_large = RobertaModel.from_pretrained('roberta-large',
+                                                return_dict=True,
+                                                output_hidden_states=True)
+
+# Freeze model
+for param in model_phobert_large.base_model.parameters():
+    param.requires_grad = False
+for param in model_xlmr_large.base_model.parameters():
+    param.requires_grad = False
 
 
 class MyEnsemble(torch.nn.Module):
@@ -15,53 +38,57 @@ class MyEnsemble(torch.nn.Module):
         self.linear1 = torch.nn.Linear(4096, 1024)
         self.linear2 = torch.nn.Linear(1024, 128)
         self.linear3 = torch.nn.Linear(128, 3)
-        self.rdrsegmenter = VnCoreNLP("/kaggle/working/vncorenlp/VnCoreNLP-1.1.1.jar", annotators="wseg", max_heap_size='-Xmx500m')
+
+        if checkvncorenlp(os.getcwd()):
+            self.rdrsegmenter = VnCoreNLP(os.getcwd() + "/vncorenlp/VnCoreNLP-1.1.1.jar",
+                                          annotators="wseg",
+                                          max_heap_size='-Xmx500m')
 
     def forward(self, en, vi):
         vector_en = self._output_xlmr(en)
         vector_vi = self._output_phobert(vi)
         x = self._matching(vector_en, vector_vi)
         x = self.linear1(x)
+        x = F.relu(x)
         x = self.linear2(x)
+        x = F.relu(x)
         x = self.linear3(x)
         return x
-
-    def _phobert(self):
-        """Pho-Bert (large)
-        """
-        self.model_phobert_large = AutoModel.from_pretrained("vinai/phobert-large",
-                                                             return_dict=True,
-                                                             output_hidden_states=True)
-        self.tokenizer_phobert_large = AutoTokenizer.from_pretrained("vinai/phobert-large", use_fast=False)
-        for param in self.model_phobert_large.base_model.parameters():
-            param.requires_grad = False
-
-    def _xlmr(self):
-        # XLM-R (original)
-        self.tokenizer_xlmr_large = RobertaTokenizer.from_pretrained('roberta-large')
-        self.model_xlmr_large = RobertaModel.from_pretrained('roberta-large', return_dict=True, output_hidden_states=True)
-
-        for param in self.model_xlmr_large.base_model.parameters():
-            param.requires_grad = False
 
     def _output_phobert(self, text):
         """Output from phobert model
         """
-        sentences = self.rdrsegmenter.tokenize(text)
-        input_ids = torch.tensor(
-            [self.tokenizer_phobert_large.encode(' '.join(list(flatten(sentences))))]
-            )
+        def batch_process(list_text):
+            sent = map(lambda x: ' '.join(list(flatten(x))),
+                       [self.rdrsegmenter.tokenize(i) for i in list_text])
+            inputs = tokenizer_phobert_large(list(sent),
+                                             return_tensors="pt",
+                                             truncation=True,
+                                             padding=True).to(self.device)
+            return inputs
+
+        if isinstance(text, str):
+            sentences = self.rdrsegmenter.tokenize(text)
+            inputs = tokenizer_phobert_large(' '.join(list(flatten(sentences))),
+                                             return_tensors="pt",
+                                             truncation=True,
+                                             padding=True).to(self.device)
+        elif isinstance(text, list):
+            inputs = batch_process(text)
 
         with torch.no_grad():
-            output_phobert = self.model_phobert_large(input_ids.to(self.device))
+            output_phobert = model_phobert_large(**inputs)
 
         return output_phobert.pooler_output
 
     def _output_xlmr(self, text):
         """Output from xlmr model
         """
-        inputs = self.tokenizer_xlmr_large(text, return_tensors="pt").to(self.device)
-        outputs = self.model_xlmr_large(**inputs)
+        inputs = tokenizer_xlmr_large(text,
+                                      return_tensors="pt",
+                                      truncation=True,
+                                      padding=True).to(self.device)
+        outputs = model_xlmr_large(**inputs)
 
         return outputs.pooler_output
 
@@ -75,4 +102,4 @@ class MyEnsemble(torch.nn.Module):
         ----
          -
         """
-        return torch.cat((en, vi, torch.abs(en-vi), torch.mul(en, vi)), dim=1)
+        return torch.cat((en, vi, torch.abs(en-vi), torch.mul(en, vi)), dim=-1)

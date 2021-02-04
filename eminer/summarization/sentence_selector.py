3aa22156c3a02790a15c47387d6073c152a89fbb
VietNDuc <viet.nguyen1123@gmail.com>
2021-02-04 15:57:37 +0700

    return top-k sentences given articles and entailment label

--- eminer/summarization/sentence_selector.py
+++ eminer/summarization/sentence_selector.py
@@ -1,10 +1,13 @@
 def select(articles, entailment_label, top_k=5):
-    """
+    """Select top_k relevant sentences given articles and label
 
     :param articles: list of articles
     :param top_k: number of selected sentences
     :param entailment_label:
     :return: top_k indices in the sentences list
     """
-    # TODO: to be implemented
-    pass
+    rs = []
+    for article in articles:
+        rs.extend(articles['relevant_sentences'])
+
+    return sorted(rs, key=lambda x: x["entailment"][entailment_label], reverse=True)[:top_k]

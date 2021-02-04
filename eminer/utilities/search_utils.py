51f9d7ce7ac19890e0d1fb93940787d56fb0a7ac
VietNDuc <viet.nguyen1123@gmail.com>
2021-02-04 15:50:33 +0700

    Add search function

---
+++ eminer/utilities/search_utils.py
@@ -0,0 +1,57 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+
+import urllib.request
+import urllib.error
+import urllib.parse
+from urllib.parse import urlencode
+from fake_useragent import UserAgent
+
+
+def _get_search_url(query, site, num=10, lang='en', search_engine="google"):
+    """Get search url
+    """
+    if search_engine.lower() == 'google':
+        params_gg = {
+            'as_sitesearch': site if site else None,
+            'lr': lang,
+            'q': query.encode('utf8'),
+            'num': num
+        }
+        params = urlencode(params_gg)
+    elif search_engine.lower() == 'bing':
+        if site is not None:
+            query = r'site:{} {}'.format(site, query)
+        params_bi = {
+            'language': lang,
+            'q': query.encode('utf8'),
+            'count': num
+        }
+        params = urlencode(params_bi)
+
+    if lang == 'vn':
+        url = u'https://www.{}.com.vn/search?'.format(search_engine)
+    elif lang == 'en':
+        url = u'https://www.{}.com/search?'.format(search_engine)
+    url += params
+
+    return url
+
+
+def _get_html(url):
+    """Get html from search url
+    """
+    # ua = UserAgent()
+    # header = ua.random
+    header = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
+
+    try:
+        request = urllib.request.Request(url)
+        request.add_header("User-Agent", header)
+        html = urllib.request.urlopen(request).read()
+        return html
+    except Exception as e:
+        print("Error accessing:", url)
+        print(e)
+        return None

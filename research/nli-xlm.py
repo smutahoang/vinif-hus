9b9cb8d92f4f0a896e9fadf0626d72717c4faee9
ngoquyenbg <35372946+ngoquyenbg@users.noreply.github.com>
2021-02-27 10:18:51 +0700

    Update nli-xlm.py

--- research/nli-xlm.py
+++ research/nli-xlm.py
@@ -1,152 +1,120 @@
-import pickle
-from transformers import XLMRobertaConfig,XLMRobertaForSequenceClassification,XLMRobertaTokenizer
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-import sentencepiece
-
-
-import torch
-
-# Load the tokenizer. 
-xlmr_tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-base" )
-
-xlmr_config = XLMRobertaConfig.from_pretrained(
-    "xlm-roberta-base", from_tf=False, num_labels = 3, output_hidden_states=False,
-)
-xlmr_model = XLMRobertaForSequenceClassification.from_pretrained(
-    "xlm-roberta-base",
-    config=xlmr_config
-)
-
-device = torch.device('cuda')
-xlmr_model.to(device)
-
-
-train_inputs = pickle.load(open('nli-train_inputs.pkl','rb'))
-val_inputs = pickle.load(open('nli-val_inputs.pkl','rb'))
-train_masks = pickle.load(open('nli-train_masks.pkl','rb'))
-val_masks = pickle.load(open('nli-val_masks.pkl','rb'))
-train_labels = pickle.load(open('nli-train_labels.pkl','rb'))
-val_labels = pickle.load(open('nli-val_labels.pkl','rb'))
-label_to_idx = pickle.load(open('nli-label_to_idx.pkl','rb'))
-idx_to_label = pickle.load(open('nli-idx_to_label.pkl','rb'))
-
-
-
-
-train_data = TensorDataset(train_inputs, train_masks, train_labels)
-train_sampler = SequentialSampler(train_data)
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=64)
-
-val_data = TensorDataset(val_inputs, val_masks, val_labels)
-val_sampler = SequentialSampler(val_data)
-val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=64)
-
 import numpy as np
-from sklearn.metrics import f1_score, accuracy_score
+from transformers import XLMRobertaConfig, XLMRobertaForSequenceClassification
+import torch
+import pickle
+from transformers import BertTokenizer
+import os
+import gdown
 
-def flat_accuracy(preds, labels):
-    pred_flat = np.argmax(preds, axis=1).flatten()
-    labels_flat = labels.flatten()
-    
-    F1_score = f1_score(pred_flat, labels_flat, average='macro')
-    
-    return accuracy_score(pred_flat, labels_flat), F1_score
+class NliXlmrClassifier:
 
+    def __init__(self, trained_model_path: str,idx_to_label:dict, device='cpu') -> None:
 
+        if not os.path.exists(trained_model_path):
+            print('Model does not exist -> downloads default model...')
+            url = 'https://drive.google.com/uc?id=190d73Qm4vB6_lu7yztsF7OH-iZtY-Ayo'
+            output = 'NLI_XLMR_100Epoch_full.bk'
+            gdown.download(url, output, quiet=False)
+            trained_model_path = 'NLI_XLMR_100Epoch_full.bk'
 
+        if not idx_to_label:
+            print('download default labels map...')
+            url = 'https://drive.google.com/uc?id=1-8UglnyNlIrhSFWUKuntwxoxVkaiOl6Z'
+            output = 'nli-idx_to_label_full.pkl'
+            gdown.download(url, output, quiet=False)
 
+            idx_to_label = pickle.load(open(r"nli-idx_to_label_full.pkl", 'rb'))
 
+        self.device = device
+        self.idx_to_label = idx_to_label
 
-import random
-from tqdm import tqdm_notebook, tqdm
-import torch.nn as nn
-from transformers import AdamW
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 
-epochs = 20
+        print('Load trained model to device..')
+        trained_model = torch.load(trained_model_path, map_location=device)
+        xlmr_config = XLMRobertaConfig.from_pretrained(
+            "xlm-roberta-base", from_tf=False, num_labels=3, output_hidden_states=False,
+        )
+        self.model = XLMRobertaForSequenceClassification(config=xlmr_config)
+        self.model.load_state_dict(trained_model['model_state_dict'])
+        #self.model.to(self.device)
+        self.model.eval
+        print('Load done !')
 
-param_optimizer = list(xlmr_model.named_parameters())
-no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-optimizer_grouped_parameters = [
-    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-]
-
-optimizer = AdamW(optimizer_grouped_parameters, lr=1e-5, correct_bias=False)
-
-
-for epoch_i in range(0, epochs):
-    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
-    print('Training...')
-
-    total_loss = 0
-    xlmr_model.train()
-    train_accuracy = 0
-    nb_train_steps = 0
-    train_f1 = 0
-    
-    for step, batch in tqdm(enumerate(train_dataloader)):
-        b_input_ids = batch[0].to(device)
-        b_input_mask = batch[1].to(device)
-        b_labels = batch[2].to(device)
-
-        xlmr_model.zero_grad()
-        outputs = xlmr_model(b_input_ids, 
-            token_type_ids=None, 
-            attention_mask=b_input_mask, 
-            labels=b_labels)
-        loss = outputs[0]
-        total_loss += loss.item()
+    def make_order(self, s1, s2):
+        if s1 > s2:
+            return s2, s1
         
-        logits = outputs[1].detach().cpu().numpy()
-        label_ids = b_labels.to('cpu').numpy()
-        tmp_train_accuracy, tmp_train_f1 = flat_accuracy(logits, label_ids)
-        train_accuracy += tmp_train_accuracy
-        train_f1 += tmp_train_f1
-        nb_train_steps += 1
+        return s1, s2
+
+    def make_input_data(self, pairs):
+        inputs = []
+        input_ids = []
+        input_masks = []
+        for pair in pairs:
+            s1, s2 = self.make_order(pair[0],pair[1])
+            text = s1 +' SEP] '+s2
+            inputs.append(text)
+            encoded_input = self.tokenizer(text, return_tensors='pt',padding="max_length",truncation=True, max_length=80)
+            input_ids.append(encoded_input['input_ids'])
+            input_masks.append(encoded_input['attention_mask'])
+
+        input_ids = torch.cat(input_ids)
+        input_masks = torch.cat(input_masks)
         
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(xlmr_model.parameters(), 1.0)
-        optimizer.step()
-        
-    avg_train_loss = total_loss / len(train_dataloader)
-    print(" Accuracy: {0:.4f}".format(train_accuracy/nb_train_steps))
-    print(" F1 score: {0:.4f}".format(train_f1/nb_train_steps))
-    print(" Average training loss: {0:.4f}".format(avg_train_loss))
+        return inputs, input_ids, input_masks
 
-    print("Running Validation...")
-    xlmr_model.eval()
-    eval_loss, eval_accuracy = 0, 0
-    nb_eval_steps, nb_eval_examples = 0, 0
-    eval_f1 = 0
-    for batch in tqdm(val_dataloader):
 
-        batch = tuple(t.to(device) for t in batch)
+    def classify(self, pairs):
 
-        b_input_ids, b_input_mask, b_labels = batch
+        inputs, input_ids, input_masks = self.make_input_data(pairs)
 
         with torch.no_grad():
-            outputs = xlmr_model(b_input_ids, 
-            token_type_ids=None, 
-            attention_mask=b_input_mask)
-            logits = outputs[0]
-            logits = logits.detach().cpu().numpy()
-            label_ids = b_labels.to('cpu').numpy()
+            outputs = self.model(input_ids, token_type_ids=None, attention_mask=input_masks)
 
-            tmp_eval_accuracy, tmp_eval_f1 = flat_accuracy(logits, label_ids)
+        logits = outputs[0]
+        logits = logits.detach().cpu().numpy()
+        pred_flat = np.argmax(logits, axis=1).flatten()
+        #print(logits.shape)
+        result = [self.idx_to_label[idx] for idx in pred_flat]
 
-            eval_accuracy += tmp_eval_accuracy
-            eval_f1 += tmp_eval_f1
-            nb_eval_steps += 1
-    print(" Accuracy: {0:.4f}".format(eval_accuracy/nb_eval_steps))
-    print(" F1 score: {0:.4f}".format(eval_f1/nb_eval_steps))
-print("Training complete!")
+        return result
 
 
-def save(model, optimizer, path):
-    # save
-    torch.save({
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict()
-    }, path)
 
-save(BERT_NLI, optimizer,'NLI_XLMR_20Epoch.bk')
+if __name__ == '__main__':
+
+
+    data = pickle.load(open(r'/home/hoang/qnt/snli_1.0_translated.pkl','rb'))
+    data = [d for d in data if d['label'] !='-']
+
+    
+
+    idx_to_label = pickle.load(open(r"/home/hoang/qnt/data_nli/nli-idx_to_label_full.pkl", 'rb'))
+
+    nli_xlmr_classifier = NliXlmrClassifier(r'/home/hoang/qnt/NLI_XLMR_100Epoch_full.bk',idx_to_label,device='cuda')
+    # nli_xlmr_classifier = NliXlmrClassifier(r'/home/hoang/qnt/NLI_XLMR_100Epoch_full.bk',idx_to_label,device='cpu')
+    makeOrder = nli_xlmr_classifier.make_order
+    xdata = []
+    labels = []
+
+    for i in range(10):
+        s1, s2 = makeOrder(data[i]['sentence1'],data[i]['sentence2'])
+        t1, t2 = makeOrder(data[i]['sentence1_translated'],data[i]['sentence2_translated'])
+        ms1, ms2 = makeOrder(data[i]['sentence1_translated'],data[i]['sentence2'])
+        mt1, mt2 = makeOrder(data[i]['sentence1'],data[i]['sentence2_translated'])
+
+        xdata.append((s1,s2))
+        labels.append(data[i]['label'])
+        xdata.append((t1,t2))
+        labels.append(data[i]['label'])
+        xdata.append((ms1,ms2))
+        labels.append(data[i]['label'])
+        xdata.append((mt1,mt2))
+        labels.append(data[i]['label'])
+
+    result = nli_xlmr_classifier.classify(xdata)
+
+    for i in range(len(xdata)):
+        print(xdata[i])
+        print('Gold [%s] vs Predict [%s] => [%r]' % (labels[i],result[i], labels[i]==result[i]))
